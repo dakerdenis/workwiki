6ebb040e76b9e85fa1d55436f385d9722fdc9b17
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-14 14:25:31 +0400

    add

---
+++ include/navigation.php
@@ -0,0 +1,13 @@
+<div class="navigation__wiki_wrapper">
+    <div class="navigation__container">
+        <div class="navigation__container_logo"></div>
+        <div class="navigation__container__login">
+            <div class="navigation__container_username">
+                snasirov
+            </div>
+            <div class="navigation__container_logout">
+                <a href="#">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php include './include/header.php'; ?>
+
 <body>
-    
+<?php include './include/navigation.php' ?>
+    <div class="wiki__wrapper">
+        <div class="wiki_infoblock">
+            <div class="wiki__infoblock_test">
+                A-Group Wiki
+            </div>
+            <div class="wiki_infoblock_content">
+                
+            </div>
+        </div>
+    </div>
+
+
+    <?php include './include/footer.php'; ?>
 </body>
+
 </html>
