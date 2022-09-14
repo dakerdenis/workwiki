86adcb63320e75de8e8f57db398eb854c363df58
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-14 15:07:57 +0400

    add

--- index.php
+++ index.php
@@ -1,29 +1,41 @@
 <?php include './include/header.php'; ?>
-<?php include './db/connection.php' ?>
+<?php include './db/connection.php'; ?>
+
 <body>
-<?php include './include/navigation.php' ?>
+    <?php include './include/navigation.php' ?>
     <div class="wiki__wrapper">
         <div class="wiki_infoblock">
             <div class="wiki__infoblock_test">
                 A-Group Wiki
             </div>
             <div class="wiki_infoblock_content">
-                <?php 
 
-$query = "SELECT * FROM category";
-$select_category = mysqli_query($connection, $query);
+                <div class="infoblock__category_wrapper">
+                    <?php
 
-
-
-while ($row = mysqli_fetch_assoc($select_category)) {
-    $id = $row['id'];
-    $clinic_name = $row['category_name'];
-    $clinic_numbers = $row['category_access'];
+                    $query = "SELECT * FROM category";
+                    $select_category = mysqli_query($connection, $query);
 
 
-?>
-               
-            
+
+                    while ($row = mysqli_fetch_assoc($select_category)) {
+                        $id = $row['id'];
+                        $category_name = $row['category_name'];
+                        $category_access = $row['category_access'];
+
+                    ?>
+                    <div class="category__wrapper__block">
+                        <a href="#"><?php echo $category_name; ?></a>
+                    </div>
+                        
+                    <?php
+                    }
+
+                    ?>
+                </div>
+
+
+
             </div>
         </div>
     </div>
