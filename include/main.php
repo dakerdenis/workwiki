206dc55a66665f69e69dfd129229c0b9a429ad6c
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-16 10:43:10 +0400

    add

--- include/main.php
+++ include/main.php
@@ -1,36 +1,63 @@
 <div class="wiki__wrapper">
-        <div class="wiki_infoblock">
-            <div class="wiki__infoblock_test">
-                A-Group Wiki
+    <div class="wiki_infoblock">
+        <div class="wiki__infoblock_test">
+            A-Group Wiki
+        </div>
+        <div class="wiki_infoblock_content">
+
+            <div class="infoblock__category_wrapper">
+                <?php
+
+                $query = "SELECT * FROM category";
+                $select_category = mysqli_query($connection, $query);
+
+
+
+                while ($row = mysqli_fetch_assoc($select_category)) {
+                    $id = $row['id'];
+                    $category_name = $row['category_name'];
+                    $category_access = $row['category_access'];
+
+                ?>
+
+                    <div class="sitemap__item">
+                        <a href="#"><?php echo $category_name; ?></a>
+                        <ul class="sitemap__subitem">
+                            <li>
+                                <a title="Şirkət haqqında" href="https://www.roimedpharma.az/az/pages/about-company">Şirkət haqqında</a>
+                            </li>
+                            <li>
+                                <a title="Fəaliyyətimiz" href="https://www.roimedpharma.az/az/pages/activities">Fəaliyyətimiz</a>
+                            </li>
+                            <li>
+                                <a title="Hədəf - Missiya - Gələcəyə baxışımız" href="https://www.roimedpharma.az/az/pages/targets-mission-our-vision">Hədəf - Missiya - Gələcəyə baxışımız</a>
+                            </li>
+                            <li>
+                                <a title="Hədəf - Missiya - Gələcəyə baxışımız" href="https://www.roimedpharma.az/az/pages/targets-mission-our-vision">Hədəf - Missiya - Gələcəyə baxışımız</a>
+                            </li>
+                            <li>
+                                <a title="Hədəf - Missiya - Gələcəyə baxışımız" href="https://www.roimedpharma.az/az/pages/targets-mission-our-vision">Hədəf - Missiya - Gələcəyə baxışımız</a>
+                            </li>
+                            <li>
+                                <a title="Hədəf - Missiya - Gələcəyə baxışımız" href="https://www.roimedpharma.az/az/pages/targets-mission-our-vision">Hədəf - Missiya - Gələcəyə baxışımız</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
+
+
+
+
+                <?php
+                }
+
+                ?>
             </div>
-            <div class="wiki_infoblock_content">
-
-                <div class="infoblock__category_wrapper">
-                    <?php
-
-                    $query = "SELECT * FROM category";
-                    $select_category = mysqli_query($connection, $query);
-
-                    
-
-                    while ($row = mysqli_fetch_assoc($select_category)) {
-                        $id = $row['id'];
-                        $category_name = $row['category_name'];
-                        $category_access = $row['category_access'];
-
-                    ?>
-                        <div class="category__wrapper__block">
-                            <a href="#"><?php echo $category_name; ?></a>
-                        </div>
-
-                    <?php
-                    }
-
-                    ?>
-                </div>
 
 
 
-            </div>
         </div>
     </div>
+</div>
