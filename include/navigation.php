a7148e5685cfe5e4988ad1855090283c83242bcb
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-16 16:08:13 +0400

    add

--- include/navigation.php
+++ include/navigation.php
@@ -1,6 +1,6 @@
 <div class="navigation__wiki_wrapper">
     <div class="navigation__container">
-        <a href="#" class="navigation__container_logo">
+        <a href="../index.php" class="navigation__container_logo">
         </a>
         <div class="navigation__container__login">
             <div class="navigation__container_username">
