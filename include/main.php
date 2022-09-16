289cf8d012347c5cbb63784fd4d3cd673e5f3c79
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-16 10:50:18 +0400

    add

--- include/main.php
+++ include/main.php
@@ -23,9 +23,22 @@
                     <div class="sitemap__item">
                         <a href="#"><?php echo $category_name; ?></a>
                         <ul class="sitemap__subitem">
+
+                        <?php
+                            $query_sub = "SELECT * FROM sub_category WHERE id = '{$id}'";
+                            $select_sub_category = mysqli_query($connection, $query_sub);
+                            while ($row = mysqli_fetch_assoc($select_sub_category)){
+
+                            }
+                        ?>
+
                             <li>
                                 <a title="Şirkət haqqında" href="https://www.roimedpharma.az/az/pages/about-company">Şirkət haqqında</a>
                             </li>
+
+
+
+
                             <li>
                                 <a title="Fəaliyyətimiz" href="https://www.roimedpharma.az/az/pages/activities">Fəaliyyətimiz</a>
                             </li>
