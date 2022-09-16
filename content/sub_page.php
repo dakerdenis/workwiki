a6e8cbf981eb837cedd4a125584d0ac18aff1ec4
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-16 11:26:12 +0400

    add

--- content/sub_page.php
+++ content/sub_page.php
@@ -1,44 +1,59 @@
 <?php
-    include '../include/header.php';
-    include '../include/navigation.php';
-    ?>
+include '../include/header.php';
+include '../include/navigation.php';
+?>
 <style>
     <?php
-        include '../styles/style.css';
+    include '../styles/style.css';
     ?>
 </style>
 
 <?php
 
-    if(isset($_GET['sub_id'])){
-        $sub_getid=$_GET['sub_id'];
+if (isset($_GET['sub_id'])) {
+    $id = $_GET['sub_id'];
 
-        $query = "SELECT * FROM sub_category WHERE id = '{$sub_getid}'";
-        $select_sub_category = mysqli_query($connection, $query);
+    $query_subpage = "SELECT * FROM sub_category WHERE id = '{$id}'";
+    $select_sub_category = mysqli_query($connection, $query_subpage);
 
 
 
-        while ($row = mysqli_fetch_assoc($select_sub_category)) {
-            $id = $row['id'];
-            $sub_id = $row['id'];
-            $sub_name = $row['name'];
-            $sub_content=$row['content'];
-          }
+    while ($row = mysqli_fetch_assoc($select_sub_category)) {
+        $id = $row['id'];
+        $sub_id = $row['id'];
+        $sub_name = $row['name'];
+        $sub_content = $row['content'];
+        $image = $row['image'];
+    }
 
 ?>
 
-<div class="sub__page__wrapper">
-    <div class="sub__page__content">
-        <div class="sub_page_desc">
-            <div class="sub_page_desc-name">
-                <?php ?>
+    <div class="sub__page__wrapper">
+        <div class="sub_page_container">
+            <!------------>
+            <div class="sub_page_desc">
+                <!--название статьи--->
+                <div class="sub_page_desc-name">
+                    <?php echo $sub_name; ?>
+                </div>
+                <!---номер статьи в БД------>
+                <div class="sub_page_desc-number">
+                    Номер статьи: <span><?php echo $id; ?></span>
+                </div>
+            </div>
+            <!------------>
+
+            <div class="sub_page_content">
+                <div class="sub_page_text">
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<?php 
-
+<?php
     include '../include/footer.php';
-?>
+} else{
+    header("Location: ../index.php");
+} ?>
