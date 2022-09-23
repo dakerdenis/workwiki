9c8a76c48acc16c5c9e9783794ab00ed976db0e9
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-09-23 14:55:33 +0400

    add

--- settings/add_user.php
+++ settings/add_user.php
@@ -1,1 +1,16 @@
-pizda
+<div class="users_wrapper">
+    <div class="users__container">
+        <div class="users__content_desc">
+            Список всех пользователей
+        </div>
+
+
+        <div class="users__content__addnew">
+            добавить нового
+        </div>
+
+        <div class="users__listofall">
+            
+        </div>
+    </div>
+</div>
