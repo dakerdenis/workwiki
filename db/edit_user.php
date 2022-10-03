b7bb339980f723aa4f181c3db4c3d49e49e0cec2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-03 11:15:29 +0400

    add

--- db/edit_user.php
+++ db/edit_user.php
@@ -1,4 +1,3 @@
-
 <?php
 $user_id =  $_GET['user_id'];
 ?>
@@ -9,47 +8,75 @@ $query_user = "SELECT * FROM users WHERE id = '{$user_id}'";
 $select_user = mysqli_query($connection, $query_user);
 while ($row = mysqli_fetch_assoc($select_user)) {
     $username = $row['username'];
+    $user_id = $row['id'];
     $password = $row['password'];
     $role = $row['role'];
     $categories = $row['categories'];
 ?>
-<form action="./db/edit_user_code.php" class="form__add_user" method="post">
-    <div class="add_user__form_name">
-        Редактирование пользователя
-    </div>
-    <div class="add_user__form__block">
-        <div class="add_user__form_desc">
-            Username:
+    <form action="./db/edit_user_code.php" class="form__add_user" method="post" style="width: 80%;">
+        <div class="add_user__form_name">
+            Редактирование пользователя
         </div>
-        <div class="add_user__form_input">
-            <input type="text" name="username" id="username" value="<?php echo $username; ?>">
+        <div class="add_user__form__block">
+            <div class="add_user__form_desc">
+                Username:
+            </div>
+            <div class="add_user__form_input">
+                <input type="text" name="username" id="username" value="<?php echo $username; ?>">
+            </div>
         </div>
-    </div>
-    <div class="add_user__form__block">
-        <div class="add_user__form_desc">
-            Role:
+    <input type="hidden" name="user_id" id="user_id" value="<?php echo $user_id; ?>">
+        <div class="add_user__form__block">
+            <div class="add_user__form_desc">
+                Role:
+            </div>
+            <div class="add_user__form_input">
+                <select name="role" id="role">
+                    <option value="<?php echo $role; ?>"><?php echo $role; ?></option>
+                    <option value="admin">admin</option>
+                    <option value="user">user</option>
+                </select>
+            </div>
         </div>
-        <div class="add_user__form_input">
-            <select name="role" id="role">
-                <option value="<?php echo $role;?>"><?php echo $role;?></option>
-                <option value="admin">admin</option>
-                <option value="user">user</option>
-            </select>
+        <div class="add_user__form__block">
+            <div class="add_user__form_desc">
+                password:
+            </div>
+            <div class="add_user__form_input">
+                <input type="text" name="password" id="password" value="<?php echo $password; ?>">
+            </div>
         </div>
-    </div>
-    <div class="add_user__form__block">
-        <div class="add_user__form_desc">
-            password:
-        </div>
-        <div class="add_user__form_input">
-            <input type="text" name="password" id="password" value="<?php echo $password; ?>">
-        </div>
-    </div>
 
-    <div class="users__content__addnew">
-        <button type="submit" id="submit" name="submit">сохранить изменения </button>
-    </div>
-</form>
+        <div class="edit__user__categories">
+            <div class="edit__user__categories_name">
+                Доступ к разделам:
+            </div>
+            <div class="edit_user__categories__wrapper">
+                <?php
+                $query = "SELECT * FROM category";
+                $select_category = mysqli_query($connection, $query);
+                while ($row = mysqli_fetch_assoc($select_category)) {
+                    $id = $row['id'];
+                    $category_name = $row['category_name'];
+                    $category_access = $row['category_access'];
+                ?>
+                    <div class="category__user__element">
+                        <input type="checkbox" id="<?php echo $id; ?>" name="<?php echo $id; ?>" value="<?php echo $category_name; ?>">
+                        <label for="<?php echo $id; ?>"> <?php echo $category_name; ?></label><br>
+                    </div>
+
+                <?php
+                }
+                ?>
+            </div>
+
+        </div>
+
+
+        <div class="users__content__addnew">
+            <button type="submit" id="submit" name="submit">сохранить изменения </button>
+        </div>
+    </form>
 <?php
 }
 ?>
