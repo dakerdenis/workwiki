7878b99217c5b53b08f417bbc3b18a593852b784
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-06 11:17:24 +0400

    add

--- content/search_result.php
+++ content/search_result.php
@@ -1,12 +1,20 @@
 <?php
-   $suka =  $_POST['search_element'];
+   $search_text =  $_POST['search_element'];
+
+   $search_text = mysqli_real_escape_string($connection, $search_text);
+
+
+
+   $query_search = "SELECT * FROM `sub_category` WHERE `key_words` LIKE '$search_text'";
+   $search__result = mysqli_query($connection, $query_search);
+
 ?>
 <div class="search__result_wrapper">
     <div class="search__result__name">
-    Результаты поиска:
+    Результаты поиска
     </div>
     <div class="search__result__by">
-        поиск по запросу:  <?php echo $suka; ?>
+        <span>поиск по запросу: </span> <?php echo $search_text; ?>
     </div>
     <div class="search__result__wrapper">
         <div class="serach__result__element">
@@ -14,10 +22,19 @@
                 Название статьи
             </div>
             <div class="serach__result__element__desc">
-                номер статьи и ключевые слова
+                ключевые слова: <span></span>
             </div>
             <div class="serach__result__element__text">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ut atque tenetur sit? Architecto omnis dolore nemo fuga sed similique, dolor aliquam ratione rem quos odio cumque voluptatum minima autem!
+
+                </p>
             </div>
             <div class="serach__result__element__readmore">
                 <a href="#">Подробнее</a>
