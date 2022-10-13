bd55e8cf48cf9be377381bbf0aed9299ba0af584
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-13 09:04:43 +0400

    add

--- content/search_result.php
+++ content/search_result.php
@@ -4,13 +4,12 @@ $search_text =  $_POST['search_element'];
 
 $search_text = mysqli_real_escape_string($connection, $search_text);
 
+$query_search = "SELECT * FROM `sub_category` WHERE `key_words` LIKE '%$search_text%'";
+$search_result = mysqli_query($connection, $query_search);
 
-$user_acces = $_SESSION['username'];
-$user_search_acces = "SELECT * FROM `users` WHERE `username` LIKE '%$user_acces%'";
-$user_search_accesresult = mysqli_query($connection, $user_search_acces);
-$row = mysqli_fetch_assoc($user_search_accesresult);
-$user_categories = $row['categories'];
-$user_list_categories = explode(",", $user_categories);
+if ($search_result == '') {
+    $result = 'К сожалению в нашей базе нет такой информации,повторите попытку уточнив поиск';
+}
 ?>
 <div class="search__result_wrapper">
     <div class="search__result__name">
@@ -20,59 +19,28 @@ $user_list_categories = explode(",", $user_categories);
         <span>поиск по запросу: </span> <?php echo $search_text; ?>
     </div>
     <div class="search__result__wrapper">
-        <?php
-        $result = false;
-        $query_search = "SELECT * FROM `sub_category` WHERE `key_words` LIKE '%$search_text%'";
-        $search__result = mysqli_query($connection, $query_search);
-        if ($search__result == null) {
-            $result = 'К сожалению в нашей базе нет такой информации,повторите попытку уточнив поиск';
-            echo 'suka';
-        } else if ($search__result != null) {
-            $result == null;
-            while ($row = mysqli_fetch_assoc($search__result)) {
-                $id = $row['id'];
-                $sub_id = $row['id'];
-                $sub_name = $row['name'];
-                $sub_content = $row['content'];
-                $image = $row['image'];
-                $key_words = $row['key_words'];
+        &nbsp;
+        <div class="serach__result__element">
+            <div class="serach__result__element_name">
+                <?php  ?>Название
+            </div>
+            <div class="serach__result__element__desc">
+                ключевые слова: <span><?php  ?></span>
+            </div>
+            <div class="serach__result__element__text">
 
-                $query_sub_id = "SELECT * FROM `category` WHERE `id` = '{$id}'";
-                $query_sub_id_result = mysqli_query($connection, $query_sub_id);
-                while ($row = mysqli_fetch_assoc($query_sub_id_result)) {
-                    $sub_id = $row['id'];
+                <?php  ?> контент
+            </div>
+            <div class="serach__result__element__readmore">
+                <a title="<?php  ?>" href="./index.php?source_page=sub_page&sub_id=<?php echo $sub_id; ?>">Подробнее</a>
+            </div>
+        </div>
 
-                    $key = array_search($sub_id, $user_list_categories);
-                    if ($key != null) {
-        ?>
-                        <div class="serach__result__element">
-                            <div class="serach__result__element_name">
-                                <?php echo $sub_name ?>
-                            </div>
-                            <div class="serach__result__element__desc">
-                                ключевые слова: <span><?php echo $key_words ?></span>
-                            </div>
-                            <div class="serach__result__element__text">
-                                <p>
-                                     <?php echo $sub_content; ?> </p>
-                            </div>
-                            <div class="serach__result__element__readmore">
-                                <a title="<?php echo $sub_name; ?>" href="./index.php?source_page=sub_page&sub_id=<?php echo $sub_id; ?>">Подробнее</a>
-                            </div>
-                        </div>
-                <?php
-                    }
-                }
-                ?>
-        <?php  }
-        }
-        ?>
         <div class="search__error">
-            <?php if ($result && $row == null) {
+            <?php if(isset($result)){
                 echo $result;
-            } else if (!$result) {
-                return false;
-            } ?>
+            }
+            ?>
         </div>
     </div>
 </div>
