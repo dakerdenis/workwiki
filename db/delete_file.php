f2482b99d8227e161787b00b12cd691526f667bb
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-18 10:14:09 +0400

    added
    file deleting functions

--- db/delete_file.php
+++ db/delete_file.php
@@ -3,6 +3,17 @@
     if(isset($_GET['file_id'])){
         $id =  $_GET['file_id'];
         $sub_id = $_GET['sub_id'];
+        //found file data
+        $query_file = "SELECT * FROM `files` WHERE `id` = '{$id}'";
+        $found_file = mysqli_query($connection,$query_file);
+
+        $row = mysqli_fetch_assoc($found_file);
+        $filename = $row['name'];
+        $filepass = "../files/" . $filename;
+        unlink(realpath($filepass));
+
+
+
 
         $q = "DELETE FROM `files` WHERE `id`='{$id}'";
         $query__delete =  mysqli_query($connection, $q);
