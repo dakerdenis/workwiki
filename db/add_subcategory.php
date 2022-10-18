dce4e5bb7114b5877bd19ce622d6b484a939ea31
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-10-18 09:37:40 +0400

    add

--- db/add_subcategory.php
+++ db/add_subcategory.php
@@ -5,8 +5,8 @@
 
   // $content =  trim($content);
  
-  $query = "INSERT INTO `sub_category`(`id`, `name`, `sub_id`, `content`, `image`, `key_words`) 
-  VALUES ('','{$subcategory_name}','{$sub_id}','','','')";
+  $query = "INSERT INTO `sub_category`(`name`, `sub_id`, `content`, `image`, `key_words`) 
+  VALUES ('{$subcategory_name}','{$sub_id}','','','')";
    echo $query;
    $addnew__subcategory = mysqli_query($connection, $query);
    header('Location: ../index.php?source_page=add_subcategory');
